e13250bc9601d97f1f573269369cfe1eaf4e81ce
Shama-33 <farhatunshama@gmail.com>
2023-06-27 21:49:38 +0600

    signup

--- app/src/main/java/com/example/cardiacrecorder/SignUpActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/SignUpActivity.java
@@ -1,14 +1,220 @@
 package com.example.cardiacrecorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private EditText emailup,passwordup,passwordupconfirm,Username,phone;
+    private TextView textViewup;
+    private Button buttonuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        this.setTitle("Sign Up");
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        Username=(EditText) findViewById(R.id.username);
+        phone=findViewById(R.id.phone);
+        emailup=(EditText) findViewById(R.id.emailup);
+        passwordup=(EditText) findViewById(R.id.passwordup);
+        passwordupconfirm=(EditText) findViewById(R.id.passwordupconfirm);
+        textViewup=(TextView) findViewById(R.id.textViewup);
+        buttonuser=findViewById(R.id.buttonuser);
+
+
+
+        textViewup.setOnClickListener(this);
+        buttonuser.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.buttonuser)
+        {
+            GeneralReg();
+
+        }
+        else if (v.getId()==R.id.textViewup){
+            Intent intent=new Intent(SignUpActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    private void GeneralReg() {
+        UserRegister();
+        String email=emailup.getText().toString().trim();
+        String pass=passwordup.getText().toString().trim();
+        String Phone_no=phone.getText().toString().trim();
+        String user=Username.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+
+
+                if (task.isSuccessful()) {
+                    //push Data
+                    UserInfo userData=new UserInfo();
+//                    userData.AccountType="GeneralUser";
+//                    userData.name=user;
+//                    userData.phone=Phone_no;
+//                    userData.email=email;
+//                    userData.userid=mAuth.getUid();
+                    //userData.setAccountType("GeneralUser");
+                    userData.setUserid(mAuth.getUid());
+                    userData.setName(user);
+                    userData.setEmail(email);
+                    userData.setPhone(Phone_no);
+
+                    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("UserInfo");
+                    databaseReference.child(mAuth.getUid()).setValue(userData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(SignUpActivity.this, "Data Updated", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SignUpActivity.this, "Data NOT Updated", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+
+
+
+
+
+                    Toast.makeText(getApplicationContext(), "User Register Successful", Toast.LENGTH_SHORT).show();
+                    finish();//page wont be seen while returning
+                    Intent intent= new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+
+                else {
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                    {Toast.makeText(getApplicationContext(), "User is already registered", Toast.LENGTH_SHORT).show();}
+                    else{Toast.makeText(getApplicationContext(), "Error : "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();}
+                }
+
+
+
+
+            }
+        });
+
+    }
+
+
+    private void UserRegister()
+    {
+        String user=Username.getText().toString().trim();
+
+        if(user.isEmpty())
+        {
+            Username.setError("Enter  name");
+            Toast.makeText(SignUpActivity.this,"name not entered",Toast.LENGTH_SHORT).show();
+        }
+        if(user.length()>30)
+        {
+            Username.setError("Name must be within 8 characters");
+            Toast.makeText(SignUpActivity.this,"Name too long",Toast.LENGTH_SHORT).show();
+        }
+        String Phone_no=phone.getText().toString().trim();
+        if(Phone_no.isEmpty())
+        {
+            Username.setError("Enter phone Number");
+            Toast.makeText(SignUpActivity.this,"Phone Number not entered",Toast.LENGTH_SHORT).show();
+        }
+
+
+        String email=emailup.getText().toString().trim();
+
+        //validity of email
+        if(email.isEmpty())
+        {
+            emailup.setError("Enter email address");
+            Toast.makeText(SignUpActivity.this,"Invalid Email",Toast.LENGTH_SHORT).show();
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            emailup.setError("Enter valid email address");
+            Toast.makeText(SignUpActivity.this,"Invalid Email",Toast.LENGTH_SHORT).show();
+            emailup.requestFocus();
+            return;
+        }
+        String pass=passwordup.getText().toString().trim();
+
+        if(pass.isEmpty())
+        {
+            passwordup.setError("Enter a password");
+            Toast.makeText(SignUpActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+        }
+        if(pass.length()<6)
+        {
+            passwordup.setError("Password must be at least 6 characters long");
+            Toast.makeText(SignUpActivity.this,"Too short Password",Toast.LENGTH_SHORT).show();
+            passwordup.requestFocus();
+            return;
+        }
+        if(pass.length()>32)
+        {
+            passwordup.setError("Password must shorter than 32 characters");
+            Toast.makeText(SignUpActivity.this,"Too long Password",Toast.LENGTH_SHORT).show();
+            passwordup.requestFocus();
+            return;
+        }
+
+        String passcmp=passwordupconfirm.getText().toString().trim();
+        if(!passcmp.equals(pass))
+        {
+            passwordupconfirm.setError("Password does not match");
+            Toast.makeText(SignUpActivity.this,"Password not matched",Toast.LENGTH_SHORT).show();
+            passwordupconfirm.requestFocus();
+            return;
+        }
+
+
+
+
+
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/cardiacrecorder/UserInfo.java
@@ -0,0 +1,58 @@
+package com.example.cardiacrecorder;
+
+public class UserInfo {
+    String userid;
+    String name;
+    String phone;
+    String email;
+    float height;
+    float weight;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
