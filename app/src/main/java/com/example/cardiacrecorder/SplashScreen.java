d92b5cdf8e70096eebe3e6685548278379ad8338
Shama-33 <farhatunshama@gmail.com>
2023-06-27 15:21:30 +0600

    splashscreen

--- app/src/main/java/com/example/cardiacrecorder/SplashScreen.java
+++ app/src/main/java/com/example/cardiacrecorder/SplashScreen.java
@@ -2,13 +2,59 @@ package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends AppCompatActivity {
+
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        //remove notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
+        progressBar= findViewById(R.id.progressBar);
+
+        Thread thread = new Thread (new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+        );
+        thread.start();
+    }
+
+    public void doWork()
+    {
+        int progress;
+        for(progress =20; progress <=100; progress = progress +20)
+        {
+            try
+            {
+                progressBar.setProgress(progress);
+                Thread.sleep(1000);
+
+
+            }catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
